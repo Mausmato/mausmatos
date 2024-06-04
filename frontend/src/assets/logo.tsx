a1bbc7ee8f4e7270627b24c543ef8e25959b457f
Mausmato <Singeripad2@gmail.com>
2024-06-03 20:57:17 -0400

    feat(frontend): Hmmm

--- frontend/src/assets/logo.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import imageToAdd from "./logo2.png";
-import "../style.css" 
-
-function Logo() {
-   return <img className="nnn" src={imageToAdd} alt="Image" />;
-}
-export default Logo;
